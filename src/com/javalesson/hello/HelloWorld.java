02bf572eb7b350b194baed220f214ca11f91a154
Mariia0210 <mariadovgalenko@gmail.com>
2024-10-21 22:57:51 +0300

    created new project - HelloWorld

---
+++ src/com/javalesson/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.javalesson.hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+
+        byte b = 10;
+        System.out.println("b = "+b);
+    }
+}
