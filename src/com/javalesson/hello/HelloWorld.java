18ea4cf3676c11dcdeaa85612343774b2c8b9907
Mariia0210 <mariadovgalenko@gmail.com>
2024-10-21 23:40:50 +0300

    delete three strings

--- src/com/javalesson/hello/HelloWorld.java
+++ src/com/javalesson/hello/HelloWorld.java
@@ -5,8 +5,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
 
-        byte b = 10;
-        System.out.println("b = "+b);
-        int age;
     }
 }
